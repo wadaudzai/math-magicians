e9b3d31f0dd946b4d892f2f0a1be21bf943e8e78
wadaudzai <wadaudzai@gmail.com>
2022-09-08 22:24:45 +0500

    Add event to the program

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,30 +1,41 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {};
+        this.handleEvent = this.handleEvent.bind(this); 
+    }
+
+    handleEvent(e) {
+        this.setState((state) => calculate(state, e.target.textContent));
+    }
   render() {
+      const {total, process, next} = this.state;
     return (
       <div className='calculator-container'>
-        <div className='result-pad'>0</div>
+        <div className='result-pad'>{(total ? total : '') + (process ? process : '') + (next ? next : '')}</div>
         <div className='buttons'>
-            <button type='button'>AC</button>
-            <button type='button'>+/-</button>
-            <button type='button'>%</button>
-            <button type='button'>÷</button>
-            <button type='button'>7</button>
-            <button type='button'>8</button>
-            <button type='button'>9</button>
-            <button type='button'>*</button>
-            <button type='button'>4</button>
-            <button type='button'>5</button>
-            <button type='button'>6</button>
-            <button type='button'>-</button>
-            <button type='button'>1</button>
-            <button type='button'>2</button>
-            <button type='button'>3</button>
-            <button type='button'>+</button>
-            <button type='button'>0</button>
-            <button type='button'>.</button>
-            <button type='button'>=</button>
+            <button type='button' onClick={this.handleEvent}>AC</button>
+            <button type='button' onClick={this.handleEvent}>+/-</button>
+            <button type='button' onClick={this.handleEvent}>%</button>
+            <button type='button' onClick={this.handleEvent}>÷</button>
+            <button type='button' onClick={this.handleEvent}>7</button>
+            <button type='button' onClick={this.handleEvent}>8</button>
+            <button type='button' onClick={this.handleEvent}>9</button>
+            <button type='button' onClick={this.handleEvent}>*</button>
+            <button type='button' onClick={this.handleEvent}>4</button>
+            <button type='button' onClick={this.handleEvent}>5</button>
+            <button type='button' onClick={this.handleEvent}>6</button>
+            <button type='button' onClick={this.handleEvent}>-</button>
+            <button type='button' onClick={this.handleEvent}>1</button>
+            <button type='button' onClick={this.handleEvent}>2</button>
+            <button type='button' onClick={this.handleEvent}>3</button>
+            <button type='button' onClick={this.handleEvent}>+</button>
+            <button type='button' onClick={this.handleEvent}>0</button>
+            <button type='button' onClick={this.handleEvent}>.</button>
+            <button type='button' onClick={this.handleEvent}>=</button>
         </div>
       </div>
     );
