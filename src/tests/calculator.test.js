7355472c4f031ecf0507232e15a2e3ed86cce783
wadaudzai <wadaudzai@gmail.com>
2022-09-15 02:46:01 +0500

    Add testing library and add tests for 2 files.

---
+++ src/tests/calculator.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Calculator from '../components/calculator';
+
+describe('Check Calculator Component', () => {
+    test('renders correctly', () => {
+        const tree = renderer.create(<Calculator />).toJSON();
+        expect(tree).toMatchSnapshot()
+    })
+})
